edb4692f596f3b1c8de96e27c69efdc76a1634d2
sathya15992 <64982242+sathya15992@users.noreply.github.com>
2021-07-06 21:08:00 +0530

    Update instancespin.tf

--- instancespin.tf
+++ instancespin.tf
@@ -2,7 +2,7 @@ provider "aws" {
 region = "us-east-1"
 }
 resource "aws_instance" "web" {
-  ami           = ami-0ab4d1e9cf9a1215a
+  ami           = "ami-0ab4d1e9cf9a1215a"
   instance_type = "t2.micro"
   key_name= "donsamosa"
 
@@ -12,5 +12,5 @@ resource "aws_instance" "web" {
 }
 
 output "instance_ip_addr" {
-  value = aws_instance.server.private_ip
+  value = "aws_instance.web.private_ip"
 }
