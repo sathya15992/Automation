e3ca0d72397913d4eb29e056d493fb7f9b4838b2
sathya15992 <64982242+sathya15992@users.noreply.github.com>
2021-07-06 19:35:15 +0530

    Update instancespin.tf

--- instancespin.tf
+++ instancespin.tf
@@ -1,8 +1,16 @@
+provider "aws" {
+region = "us-east-1"
+}
 resource "aws_instance" "web" {
   ami           = ami-0ab4d1e9cf9a1215a
   instance_type = "t2.micro"
+  keys= "donsamosa"
 
   tags = {
     Name = "Terraform"
   }
+}
+
+output "instance_ip_addr" {
+  value = aws_instance.server.private_ip
 }
