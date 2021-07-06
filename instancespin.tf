cc9a09686abe60d26159b97e8aa217bd5e0fa038
sathya15992 <64982242+sathya15992@users.noreply.github.com>
2021-07-06 19:38:33 +0530

    Update instancespin.tf

--- instancespin.tf
+++ instancespin.tf
@@ -4,7 +4,7 @@ region = "us-east-1"
 resource "aws_instance" "web" {
   ami           = ami-0ab4d1e9cf9a1215a
   instance_type = "t2.micro"
-  key-name= "donsamosa"
+  key_name= "donsamosa"
 
   tags = {
     Name = "Terraform"
